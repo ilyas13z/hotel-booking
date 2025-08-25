fd261bf508c4507026c50549aca3b9685dc37057
ilyas13z <ilyas13z@yandex.ru>
2025-08-25 18:48:29 +0500

    Some fix

--- src/hotel_booking/hotel_booking/settings.py
+++ src/hotel_booking/hotel_booking/settings.py
@@ -64,12 +64,6 @@ DATABASES = {
         "PORT": "5432",
     }
 }
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
 
 AUTH_PASSWORD_VALIDATORS = [
     {
